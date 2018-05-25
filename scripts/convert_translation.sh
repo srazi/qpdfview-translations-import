38e13f891cbd76fd8de9881bf524cd1b7ea6cbcd
Razi Alavizadeh <s.r.alavizadeh@gmail.com>
2018-05-25 17:54:18 +0430

    Fix convert to translation part

--- scripts/convert_translation.sh
+++ scripts/convert_translation.sh
@@ -1,12 +1,21 @@
 source_dir=$(dirname $0)/../translations-export
 dest_dir=$(dirname $0)/../trunk/translations
 
+
 for locale in $*
 do
-	source_file=${locale}.po
-	dest_file=qpdfview_${locale}.ts
+    source_file=${locale}.po
+    dest_file=qpdfview_${locale}.ts
 
-	echo "Converting translation of locale "${locale}
-	lconvert -i ${source_dir}/${source_file} -o ${dest_dir}/${dest_file}
+    echo "Converting translation of locale "${locale}
+    sed -i "s/^\"X-Generator:..*/\"X-Qt-Contexts: true\\\\n\"/" ${source_dir}/${source_file}
+    sed -i s/2012-2016/2012-%1/ ${source_dir}/${source_file}
+    sed -i -r  "/^msgctxt \"[^Qq][^:]+\"/ s/^msgctxt \"([^Qq][^:|\"]+)|\"$/msgctxt \"qpdfview::\1/" ${source_dir}/${source_file}
+    sed -i "/^#\\s/ d" ${source_dir}/${source_file}
+    sed -i "/^#$/ d" ${source_dir}/${source_file}
+
+    $LCONVERT_CMD -i ${source_dir}/${source_file} -o ${dest_dir}/${dest_file}
+
+    sed -i "/^\\s*<extra-po-header.*/ d" ${dest_dir}/${dest_file}
+    sed -i "s/<translation>/<translation type=\"unfinished\">/" ${dest_dir}/${dest_file}
 done
-
