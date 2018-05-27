e61e97902fa7750e78b5146078aed5b0cbbed39d
Razi Alavizadeh <s.r.alavizadeh@gmail.com>
2018-05-28 02:41:11 +0430

    Change all to unfinished to minimize diff

--- scripts/convert_translation1.sh
+++ scripts/convert_translation1.sh
@@ -11,4 +11,5 @@ do
     $LCONVERT_CMD -no-obsolete -i ${source_dir}/${source_file} -o ${dest_dir}/${dest_file}
 
     python3 $(dirname $0)/fix_translation_context.py ${dest_dir}/${dest_file}
+    sed -i "s/<translation>/<translation type=\"unfinished\">/" ${dest_dir}/${dest_file}
 done
